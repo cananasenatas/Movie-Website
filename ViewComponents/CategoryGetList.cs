624d58b77bbb8bdce3b1eca3fc7264e01d9cc328
cananasenatas <tascananasena@gmail.com>
2021-09-22 17:27:19 +0300

    ViewComponents

---
+++ ViewComponents/CategoryGetList.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWebSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.ViewComponents
+{
+    public class CategoryGetList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            GenresRepository genresRepository = new GenresRepository();
+            var genreslist = genresRepository.TList();
+            return View(genreslist);
+        }
+    }
+}
---
+++ ViewComponents/MovieListByCategory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWebSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.ViewComponents
+{
+    public class MovieListByCategory : ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        {
+
+            WebsiteRepository websiteRepository = new WebsiteRepository();
+            var movielist = websiteRepository.List(x => x.GenresId == id).Where(x=>x.FilterId==1);
+            return View(movielist);
+        }
+    }
+}
