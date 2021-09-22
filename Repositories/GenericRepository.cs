bc1e9d39dd424133d8400f6d7056ebbbcc11958b
cananasenatas <tascananasena@gmail.com>
2021-09-22 17:26:42 +0300

    Repositories

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using MovieWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        Context context = new Context();
+
+
+        public List<T> TList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void TAdd(T t)
+        {
+            context.Set<T>().Add(t);
+            context.SaveChanges();
+
+        }
+        public void TDelete(T t)
+        {
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public void TUpdate(T t)
+        {
+            context.Set<T>().Update(t);
+            context.SaveChanges();
+
+        }
+
+
+        public T TGet(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return context.Set<T>().Where(filter).ToList();
+        }
+
+    }
+}
+
+
---
+++ Repositories/GenresRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.Repositories
+{
+    public class GenresRepository : GenericRepository<Genres>
+    {
+    }
+}
