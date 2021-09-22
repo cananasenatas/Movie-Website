9af1c72e9badb612cfe62d4125b763e18dfd9d05
cananasenatas <tascananasena@gmail.com>
2021-09-22 17:25:07 +0300

    Models

---
+++ Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-N1UQOJ3H; database=MovieWeb; integrated security=true");
+
+        }
+
+        public DbSet<Website> Website { get; set; }
+        public DbSet<Genres> Genres { get; set; }
+        public DbSet<Choose> Choose { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Filter> Filter { get; set; }
+        public DbSet<AppUser> AppUser { get; set; }
+    }
+}
---
+++ Models/Website.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebSite.Models
+{
+    public class Website
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int ReleaseYear { get; set; }
+        public string Description { get; set; }
+        public double Rating { get; set; }
+        public string ImageUrl { get; set; }
+        public string Director { get; set; }
+        public string Link { get; set; }
+        public int GenresId { get; set; }
+        public Genres Genres { get; set; }
+        public string Actors { get; set; }
+        public string WatchTime { get; set; }
+        public int ChooseId { get; set; }
+        public Choose Choose { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        public int FilterId { get; set; }
+        public Filter Filter { get; set; }
+
+    }
+}
